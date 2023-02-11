c280600af8dd58f32bfa7ed35ada47b5a683b379
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-02-11 19:40:49 +0800

    Done: Bubble Sort Visualizer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Name: Jerico James F. Viteño
-# Final Project: Sorting Visualizer
+# Final Project: Sorting Visualizer in Bubble Sort
 # February 18, 2023 
 
 import matplotlib.pyplot as plt
@@ -12,10 +12,11 @@ x = np.arange(0, amount, 1)
 
 n = len(list)
 
+# Bubble Sort
 for i in range(n):
     for j in range(0, n-i-1):
         plt.bar(x, list)
-        plt.pause(0.01)
+        plt.pause(0.001)
         plt.clf()
         if list[1] > list[j+1]:
             list[j], list[j+1] = list[j+1], list[j]
