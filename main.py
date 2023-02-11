9ae3a4b8840214c79d88122e3c3c3573d223a541
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-02-11 19:37:57 +0800

    Added: for loop

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ amount = 15
 list = np.random.randint(0, 100, amount)
 x = np.arange(0, amount, 1)
 
+n = len(list)
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        plt.bar(x, list)
+        plt.pause(0.01)
+        plt.clf()
+        if list[1] > list[j+1]:
+            list[j], list[j+1] = list[j+1], list[j]
+
+plt.show()
