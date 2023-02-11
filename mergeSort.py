8690c9c126056d510c6a0da697fb94f65181dc86
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-02-11 20:02:58 +0800

    Added: Merge Method with leftcounter and rightcounter conditions

--- mergeSort.py
+++ mergeSort.py
@@ -11,6 +11,7 @@ random.seed('ABC')
 numbers = [random.randint(0, 1000)]
 
 def mergeSort(numberList, left, right):
+    
     if left >= right:
         return
 
@@ -18,3 +19,33 @@ def mergeSort(numberList, left, right):
 
     mergeSort(numberList, left, mid)
     mergeSort(numberList, mid + 1, right)
+
+    merge(numberList, left, right, mid)
+
+def merge(numberList, left, right, mid):
+    
+    leftCopy = numberList[left : mid +1]
+    rightCopy = numberList[mid + 1 : right + 1]
+
+    leftCounter, rightCounter = 0, 0
+    sortedCounter = left
+
+    while leftCounter < len(leftCopy) and rightCounter < len(rightCopy):
+        if leftCopy[leftCounter] <= rightCopy[rightCounter]:
+            numberList[sortedCounter] = leftCopy[leftCounter]
+            leftCounter += 1
+        else:
+            numberList[sortedCounter] = rightCopy[rightCounter]
+            rightCounter += 1
+
+        sortedCounter += 1
+
+    while leftCounter < len(leftCopy):
+        numberList[sortedCounter] = leftCopy[leftCounter]
+        leftCounter += 1
+        sortedCounter += 1
+
+    while rightCounter < len(rightCopy):
+        numberList[sortedCounter] = rightCopy[rightCounter]
+        rightCounter += 1
+        sortedCounter += 1
